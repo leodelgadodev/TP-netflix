9fdf95cb5334e9ee784f339ae13b973a6f3cf48f
leodelgadodev <leodelgado.dev@gmaail.com>
2020-05-16 17:47:14 -0300

    sarasa

--- api/src/main/kotlin/ui/unq/edu/ar/Hello.kt
+++ api/src/main/kotlin/ui/unq/edu/ar/Hello.kt
@@ -6,4 +6,8 @@ import io.javalin.http.Context
 fun main() {
     val app = Javalin.create().start(7000)
     app.get("/") { ctx -> ctx.result("Hello World") }
+
+    fun helloWorld(): String {
+        return "asd"
+    }
 }
