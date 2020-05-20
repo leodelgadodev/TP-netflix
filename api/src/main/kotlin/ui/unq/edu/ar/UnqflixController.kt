4716e58e617940fb651e0ab5c98501f27457ed72
mendezigna <ignacio.mendez0000@gmail.com>
2020-05-20 16:38:11 -0300

    pequeños arreglos y agrego la dependecia de javalin jwt

--- api/src/main/kotlin/ui/unq/edu/ar/UnqflixController.kt
+++ api/src/main/kotlin/ui/unq/edu/ar/UnqflixController.kt
@@ -3,6 +3,7 @@ package ui.unq.edu.ar
 import data.getUNQFlix
 import domain.*
 import domain.Available
+import io.javalin.http.BadRequestResponse
 import io.javalin.http.Context
 import io.javalin.http.NotFoundResponse
 import ui.unq.edu.ar.mappers.*
@@ -33,7 +34,7 @@ class UnqflixController {
             unqFlix.addLastSeen("u_1", idMapper.id!!)
             ctx.json(mapOf("result" to "ok"))
         } catch (e : NotFoundException){
-            throw NotFoundResponse(e.toString())
+            throw BadRequestResponse(e.message!!)
         }
 
     }
@@ -56,10 +57,10 @@ class UnqflixController {
     }
 
     fun search(ctx : Context){
-        val text : String = ctx.queryParam<String>("text").get()
+        val text : String? = ctx.queryParam("text")
         val contents : MutableList<ContentViewMapper> = mutableListOf()
-        contents.addAll(unqFlix.searchSeries(text).map{ ContentViewMapper(it.id, it.title, it.description, it.state.javaClass === Available().javaClass) })
-        contents.addAll(unqFlix.searchMovies(text).map{ ContentViewMapper(it.id, it.title, it.description, it.state.javaClass === Available().javaClass) })
+        contents.addAll(unqFlix.searchSeries(text.toString()).map{ ContentViewMapper(it.id, it.title, it.description, it.state.javaClass === Available().javaClass) })
+        contents.addAll(unqFlix.searchMovies(text.toString()).map{ ContentViewMapper(it.id, it.title, it.description, it.state.javaClass === Available().javaClass) })
         ctx.json(mapOf("Contents" to contents))
     }
 
