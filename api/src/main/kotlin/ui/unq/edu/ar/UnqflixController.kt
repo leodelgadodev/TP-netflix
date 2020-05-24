faa16f540e95442e417ec5148d546c8c8ba95d49
gonzabl <gonzabl@live.com.ar>
2020-05-24 17:08:50 -0300

    Agregado el metodo get /content:id y otras modificaciones

--- api/src/main/kotlin/ui/unq/edu/ar/UnqflixController.kt
+++ api/src/main/kotlin/ui/unq/edu/ar/UnqflixController.kt
@@ -3,7 +3,6 @@ package ui.unq.edu.ar
 import data.getUNQFlix
 import domain.*
 import domain.Available
-import io.javalin.http.BadRequestResponse
 import io.javalin.http.Context
 import io.javalin.http.NotFoundResponse
 import ui.unq.edu.ar.mappers.*
@@ -55,7 +54,38 @@ class UnqflixController {
     }
 
     fun getContentById(ctx : Context){
+        var id = ctx.pathParam(":contentId")
 
+        if (id.startsWith("mov")) return getMovieContent(ctx,id)
+        if (id.startsWith("ser")) return getSerieContent(ctx,id)
+        else throw NotFoundResponse("No se ha encontrado contenido con el id = ${id}")
+    }
+
+    private fun getSerieContent(ctx: Context,id: String) {
+        val serie= unqFlix.series.find { it.id == id }
+        if (serie!=null){
+            val categories = serie.categories.map { it.name }.toMutableList()
+            val relatedContent= serie.relatedContent.map { ContentViewMapper(it.id,it.title,it.description,it.state.javaClass === Available().javaClass)}.toMutableList()
+            val season = serie.seasons.map { SeasonViewMapper(it.id,it.title,it.description,it.poster,
+                it.chapters.map{ ChapterViewMapper(it.id,it.title,it.description,it.duration,it.video,it.thumbnail)}.toMutableList())
+            }
+
+            ctx.json(SerieViewMapper(serie.id, serie.title,serie.description,serie.poster,categories ,relatedContent, season as MutableList<SeasonViewMapper>))
+        } else{
+            throw NotFoundResponse("No se ha encontrado la serie con el id = ${id}")
+        }
+    }
+    private fun getMovieContent(ctx: Context,id: String) {
+        val movie = unqFlix.movies.find { it.id == id }
+        if (movie != null){
+            val categories = movie.categories.map { it.name }.toMutableList()
+            val relatedContent = movie.relatedContent.map { ContentViewMapper(it.id,it.title,it.description,it.state.javaClass === Available().javaClass) }
+
+            ctx.json(MovieViewMapper(movie.id,movie.title,movie.description,movie.poster,movie.video,movie.duration,movie.actors,movie.directors,categories,
+                relatedContent as MutableList<ContentViewMapper>))
+        } else{
+            throw NotFoundResponse("No se ha encontrado la pelicula con el id = ${id}")
+        }
     }
 
     fun postFavById(ctx : Context){
--- api/src/main/kotlin/ui/unq/edu/ar/mappers/Mappers.kt
+++ api/src/main/kotlin/ui/unq/edu/ar/mappers/Mappers.kt
@@ -4,13 +4,15 @@ data class ContentViewMapper(val id: String, val title: String, val description 
 
 class IdMapper (val id : String?) {}
 
-data class MovieViewMapper(val id: String, val title: String, val description: String, val poster: String,val video: String,
-                           val duration: Int, val actors: MutableList<String>, val directors: MutableList<String>,
-                           val categories :MutableList<String>, val relatedContent: MutableList<ContentViewMapper>)
+data class MovieViewMapper(
+    val id: String, val title: String, val description: String, val poster: String, val video: String,
+    val duration: Int, val actors: MutableList<String>, val directors: MutableList<String>,
+    val categories: MutableList<String>, val relatedContent: MutableList<ContentViewMapper>)
 
-data class SerieViewMapper(val id: String, val title: String, val description: String, val poster: String,
-                           val categories :MutableList<String>, val relatedContent: MutableList<ContentViewMapper>,
-                           val season: MutableList<SeasonViewMapper>)
+data class SerieViewMapper(
+    val id: String, val title: String, val description: String, val poster: String,
+    val categories: MutableList<String>, val relatedContent: MutableList<ContentViewMapper>,
+    val season: MutableList<SeasonViewMapper>)
 
 data class SeasonViewMapper(val id: String, val title: String, val description: String, val poster: String,
                             val chapters: MutableList<ChapterViewMapper>)
