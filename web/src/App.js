e5f0a328cce55d1db4e3b31e1e9e589fc539e6ac
mendezigna <ignacio.mendez0000@gmail.com>
2020-06-15 16:11:11 -0300

    proyecto iniciado

---
+++ web/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+    <>
+      
+    </>
+    );
+  }
+}
+
+export default App;
