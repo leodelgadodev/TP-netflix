b96a633b0b3a8e598daeee9ce80c78086b66241d
leodelgadodev <leodelgado.dev@gmail.com>
2020-06-27 19:44:36 -0300

    fix AuthService

--- web/src/services/AuthService.js
+++ web/src/services/AuthService.js
@@ -2,16 +2,13 @@ import axios from 'axios';
 axios.defaults.baseURL = 'http://localhost:7000';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-function AuthService() {
+export const AuthService = {
     
-    const register = (name, email, password, image, creditCard) =>  {
+    register: (name, email, password, image, creditCard) =>  {
         axios.post("/register", {name, email, password, image, creditCard}, {})
-    }
+    },
     
-    const login = (email, password) => {
+    login: (email, password) => {
         axios.post("/login", {email, password})
     }
 }
-
-
-export default AuthService;
