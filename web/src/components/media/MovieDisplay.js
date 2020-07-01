3c25b718826bd7adf6a1160cd8255e1306f90129
gonzabl <gonzabl@live.com.ar>
2020-07-01 02:41:20 -0300

    cosas

--- web/src/components/media/MovieDisplay.js
+++ web/src/components/media/MovieDisplay.js
@@ -9,7 +9,7 @@ class MovieDisplay extends React.Component{
         console.log(this.props);
         return(
             <div class="media">
-                
+                <img src = {this.props.movie.poster} class="mr-3" alt={this.props.movie.title}/>
                 <div class="media-body">
                 <h5 class="mt-0">{this.props.movie.title}</h5>
                     <p>{this.props.movie.description}</p>
@@ -19,5 +19,5 @@ class MovieDisplay extends React.Component{
         )
     }
     
-}// <img src = {this.props.movie.poster} class="mr-3" alt={this.props.movie.title}/>
+}
 export default MovieDisplay;
--- web/src/components/page-components/MediaPage.js
+++ web/src/components/page-components/MediaPage.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import MovieDisplay from '../media/MovieDisplay';
 import {MediaService} from '../../services/MediaService';
+import { useParams } from 'react-router-dom';
 
 export default function MediaPage(props) {
     
     const [movie, setMovie] = useState(null);
+    const {id} = useParams();
+
     
-    useEffect(()=>{MediaService.getContent("mov_1").then(res=>{setMovie(res.data);console.log(res)})},[])
+    useEffect(()=>{MediaService.getContent(id).then(res=>{setMovie(res.data);console.log(res)})},[])
     
     return (
         <div className="home-page">
