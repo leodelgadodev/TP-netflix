22a6e0df527624bd60887f4b6271d4be765ea733
gonzabl <gonzabl@live.com.ar>
2020-07-01 18:58:46 -0300

    cambios minimos a serie

--- web/src/components/media/SerieDisplay.js
+++ web/src/components/media/SerieDisplay.js
@@ -8,11 +8,14 @@ export default function SerieDisplay(props){
         return <h2>404 not found</h2>
     } 
     return(
-        <div>
-            <h1>{media.title}</h1>
-            <img src = {media.poster} alt={media.title}/>
-            <p>{media.description}</p>
-            <h5>Seasons</h5>
+        <div className="media">
+            <img src = {media.poster} className="mr-3" alt={media.title}/>
+                <div className="media-body">
+                <h1 className="mt-0">{media.title}</h1>
+                <p className="mt-0">{media.description}</p>
+                <h5 className="mt-0">Seasons</h5>
+            </div>
+            
         </div>
     );
     
