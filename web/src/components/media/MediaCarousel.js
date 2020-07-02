2bdc0b385a841d18c287e63f922d93a1cd8d014a
mendezigna <ignacio.mendez0000@gmail.com>
2020-07-02 00:40:40 -0300

    arreglo

--- web/src/components/media/MediaCarousel.js
+++ web/src/components/media/MediaCarousel.js
@@ -13,15 +13,7 @@ export default function MediaCarousel(props) {
         // eslint-disable-next-line react/prop-types
         switch(props.flag) {
             case "favs": {
-                
                 getUser().then((res) => {
-                    // debugger; setMediaArray(res.data.favorites);
-                    // res.data.favorites.forEach((content) => {
-                    //     getContentById(content.id).then( (res) => {
-                    //         const contents = mediaArray.concat([res.data])
-                    //         setMediaArray(contents)
-                    //     })
-                    // });
                     setMediaArray(res.data.favorites);
                 });
             } break;
@@ -29,19 +21,10 @@ export default function MediaCarousel(props) {
                 getUser().then(res => {
                     setMediaArray(res.data.lastSeen);
                 })
-                // UserService.getUser().then((data) => {
-                //     debugger; setMediaArray(data.lastSeen);
-                //     debugger; console.log(data.lastSeen);
-                // })
-            } break; 
-            default: {
-                // si no envia favs o lastSeen, envia id del content
-                // eslint-disable-next-line react/prop-types
-                getContentById(props.flag).then((data) => {
-                    setMediaArray(data.relatedContent);
-                    console.log(data.relatedContent);
-                })
             } break;
+            case "relatedContent":{
+                setMediaArray(props.relatedContent)
+            } break; 
         }
     }, []);
 
@@ -50,65 +33,18 @@ export default function MediaCarousel(props) {
         return UserService.getUser();
     }
     
-    const getContentById = (id) => {
-        return MediaService.getContent(id);
-    }
-
-    const [fst, ...rest] = mediaArray
-
     if(mediaArray == []){
         return <h2>aun no tienes {props.flag} :(</h2>
     } else {
         return(
-            // <div id="carouselExampleControls" className="carousel slide media-carousel" data-ride="carousel">
-            //     <div className="carousel-inner">
-            //         <MediaButton content={fst} className="carousel-item active" />
-            //         {/* <div className="carousel-item active">
-            //             <img className="media-poster-img" src={verifyPoster(fst)} alt="First slide"/>
-            //         </div> */}
-            //             {rest.map (content => (
-            //                 <MediaButton key={content.id} content={content} className="carousel-item" />
-            //             ))}
-            //         <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-            //             <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            //             <span className="sr-only">Previous</span>
-            //         </a>
-            //         <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-            //             <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            //             <span className="sr-only">Next</span>
-            //         </a>
-            //     </div>
-            // </div>
-            <div>
-        <HorizontalScroller>
-            <div className="media-scroll-elem">1</div>
-            <div className="media-scroll-elem">2</div>
-            <div className="media-scroll-elem">3</div>
-            <div className="media-scroll-elem">4</div>
-            <div className="media-scroll-elem">5</div>
-            <div className="media-scroll-elem">6</div>
-            <div className="media-scroll-elem">7</div>
-            <div className="media-scroll-elem">8</div>
-            <div className="media-scroll-elem">1</div>
-            <div className="media-scroll-elem">2</div>
-            <div className="media-scroll-elem">3</div>
-            <div className="media-scroll-elem">4</div>
-            <div className="media-scroll-elem">5</div>
-            <div className="media-scroll-elem">6</div>
-            <div className="media-scroll-elem">7</div>
-            <div className="media-scroll-elem">8</div>
-            <div className="media-scroll-elem">1</div>
-            <div className="media-scroll-elem">2</div>
-            <div className="media-scroll-elem">3</div>
-            <div className="media-scroll-elem">4</div>
-            <div className="media-scroll-elem">5</div>
-            <div className="media-scroll-elem">6</div>
-            <div className="media-scroll-elem">7</div>
-            <div className="media-scroll-elem">8</div>
-        </HorizontalScroller>
-        </div>
+            <div className="media-carousel">
+                <HorizontalScroller>
+                    {mediaArray.map (content => (
+                        <MediaButton key={content.id} content={content} className="media-scroll-elem" />
+                    ))}
+                </HorizontalScroller>
+            </div>
         );
     }
-
     
 }
