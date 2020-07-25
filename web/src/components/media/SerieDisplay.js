85b0273a8c691142fd3b92ad92820d1e0309a4af
leodelgadodev <leodelgado.dev@gmail.com>
2020-07-25 15:32:44 -0300

    tabs chapter styling

--- web/src/components/media/SerieDisplay.js
+++ web/src/components/media/SerieDisplay.js
@@ -22,50 +22,20 @@ export default function SerieDisplay(props){
         }
     });
 
-    // const TableHeader = () => {
-    //     const rows = media.season.map((row, index) =>{ 
-    //         return(
-    //             <th  key= {index}>
-    //                 <td className="row-season">Season {index + 1}</td>
-    //             </th>
-    //         )
-    //     });
-    //     return <thead>{rows}</thead>
-    // }
-
-    // const TableBody = () => {
-    //     const rows = media.season.map((row, index) =>{
-    //         const chapters = row.chapters.map((chap, index)=>{
-    //             return(
-    //                 <tr key={index}>
-    //                     <Link to={{ pathname: `/media/${media.id}/${row.id}/${chap.id}/video` }}>
-    //                         {chap.title}
-    //                     </Link>
-    //                 </tr>
-    //             )
-    //         });
-    //         return (
-    //                 <td key= {index}>{chapters}</td>
-    //         );
-    //     });
-    //     return <tbody>{rows}</tbody>
-    // }
-
     const SeasonTab = () => {
         const rows = [];
         rows.push(media.season.map((row, index) =>{
             const chapters = row.chapters.map((chap, index)=>{
                 return(
-                    <span key={index}>
+                    <li key={index}>
                         <Link to={{ pathname: `/media/${media.id}/${row.id}/${chap.id}/video` }}>
                             {chap.title}
                         </Link>
-                    </span>
+                    </li>
                 )
             });
             const id = "nav-tab-" + index
             return (
-                // poner {index} adentro de " " no funciona y se renderiza como nav-tab-{index} D:
                 <div key={index} className="tab-pane fade" id={id} role="tabpanel" aria-labelledby={id}>{chapters}</div>
             );
         }));
@@ -80,8 +50,7 @@ export default function SerieDisplay(props){
         const rows = media.season.map((row, index) => { 
             const href = "#nav-tab-" + index
             return(
-                // poner {index} adentro de " " no funciona y se renderiza como nav-tab-{index} D:
-                <a key={index} className="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href={href} role="tab" aria-controls={href} aria-selected="false">
+                <a key={index} className="nav-item nav-link tab-header" id="nav-contact-tab" data-toggle="tab" href={href} role="tab" aria-controls={href} aria-selected="false">
                     {row.title}
                 </a>
             )
@@ -109,26 +78,9 @@ export default function SerieDisplay(props){
                     <FavButton mediaId={media.id} />
 
                     <h5 className="mt-0">Temporadas</h5>
-
-                    {/* <Tabs>
-                        <TabList>
-                        <Tab>Title 1</Tab>
-                        <Tab>Title 2</Tab>
-                        </TabList>
-
-                        <TabPanel>
-                        <h2>Any content 1</h2>
-                        </TabPanel>
-                        <TabPanel>
-                        <h2>Any content 2</h2>
-                        </TabPanel>
-                    </Tabs> */}
-                    {/* <table className="table table-sm table-dark media-table"> */}
-                        {/* <TableHeader />
-                        <TableBody /> */}
-                        <SeasonTabHeader/>
-                        <SeasonTab/>
-                    {/* </table> */}
+                    
+                    <SeasonTabHeader/>
+                    <SeasonTab/>
                 </div>
             </div>
             <div>
